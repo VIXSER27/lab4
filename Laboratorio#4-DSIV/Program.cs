2b7b8c94b32a11b3c2e9a6476c81ada2de4e604a
Javier1185 <kingdario1185@gmail.com>
2025-11-17 21:15:55 -0500

    Cambiando ruta de inico

--- Laboratorio#4-DSIV/Program.cs
+++ Laboratorio#4-DSIV/Program.cs
@@ -16,7 +16,7 @@ namespace Laboratorio_4_DSIV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Portada());
         }
     }
 }
